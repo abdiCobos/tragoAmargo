cbb440d0d8ab4818241bcd33ae4735f7f8396e8c
coboab <coboab@gitlab.com>
2026-08-27 12:00:05 -0700

    revert: move back to imgbb to avoid firebase billing constraints

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,35 +1,47 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:http/http.dart' as http;
 import '../core/utils/crash_reporting.dart';
 
 class StorageService {
-  final FirebaseStorage _storage = FirebaseStorage.instance;
+  static final String _apiKey = _loadApiKey();
+  static const String _uploadUrl = 'https://api.imgbb.com/1/upload';
 
-  Future<String> _uploadToPath(String path, Uint8List bytes) async {
+  static String _loadApiKey() {
+    if (kIsWeb) {
+      return const String.fromEnvironment('IMGBB_API_KEY', defaultValue: '');
+    }
+    return '2138a891968bd4fca786ab8cff4d1ba4';
+  }
+
+  Future<String> uploadImageBytes(Uint8List bytes, {String? name}) async {
     try {
-      final ref = _storage.ref().child(path);
-      final metadata = SettableMetadata(contentType: 'image/jpeg');
-      final snapshot = await ref.putData(bytes, metadata);
-      final url = await snapshot.ref.getDownloadURL();
-      return url;
+      final request = http.MultipartRequest('POST', Uri.parse(_uploadUrl));
+      request.fields['key'] = _apiKey;
+      if (name != null) request.fields['name'] = name;
+      request.files.add(http.MultipartFile.fromBytes(
+        'image', bytes,
+        filename: '${DateTime.now().millisecondsSinceEpoch}.jpg',
+      ));
+
+      final response = await request.send();
+      final body = jsonDecode(await response.stream.bytesToString());
+      if (body['success'] == true) {
+        return body['data']['url'] as String;
+      }
+      throw Exception(body['error']?['message'] ?? 'Error al subir imagen');
     } catch (e, stack) {
-      CrashReporting.recordError(e, stack, reason: 'StorageService._uploadToPath');
+      CrashReporting.recordError(e, stack, reason: 'StorageService.uploadImageBytes');
       rethrow;
     }
   }
 
-  Future<String> uploadShopPhoto(String shopId, Uint8List bytes) {
-    final fileName = '${DateTime.now().millisecondsSinceEpoch}.jpg';
-    return _uploadToPath('shops/$shopId/photos/$fileName', bytes);
-  }
+  Future<String> uploadShopPhoto(String shopId, Uint8List bytes) =>
+      uploadImageBytes(bytes, name: 'shop_$shopId');
 
-  Future<String> uploadProductPhoto(String shopId, Uint8List bytes) {
-    final fileName = '${DateTime.now().millisecondsSinceEpoch}.jpg';
-    return _uploadToPath('shops/$shopId/products/$fileName', bytes);
-  }
+  Future<String> uploadProductPhoto(String shopId, Uint8List bytes) =>
+      uploadImageBytes(bytes, name: 'product_$shopId');
 
-  Future<String> uploadUserPhoto(String uid, Uint8List bytes) {
-    final fileName = '${DateTime.now().millisecondsSinceEpoch}.jpg';
-    return _uploadToPath('users/$uid/$fileName', bytes);
-  }
+  Future<String> uploadUserPhoto(String uid, Uint8List bytes) =>
+      uploadImageBytes(bytes, name: 'user_$uid');
 }
